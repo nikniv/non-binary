6f6662df09fe3dcda0665beda060183745b03429
niveditn <11422270+niveditn@users.noreply.github.com>
2019-04-09 13:06:12 +0530

    Update stripe-3.f

--- stripe-3.f
+++ stripe-3.f
@@ -3,6 +3,7 @@ integer i, j
 program hello
   do i = 1, 100, 7
      j = i * i
-     print *, i, ": Hello Purple! ", j
+     print *, i, ": Hello Purple!"
+     print *, j
   end do
 end program hello
