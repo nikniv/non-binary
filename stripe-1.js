a871ffbbeaf6b9f0e77b4a31daea19dde67b89e9
niveditn <11422270+niveditn@users.noreply.github.com>
2019-04-09 12:46:16 +0530

    Update stripe-1.js

--- stripe-1.js
+++ stripe-1.js
@@ -1,1 +1,9 @@
-console.log('hello yellow');
+const printYellowStripe = (num) => {
+  let i = 0;
+  do {
+    i++;
+    console.log(`${i}: hello yellow`);
+  } while (i < num);
+}
+
+printYellowStripe(10);
