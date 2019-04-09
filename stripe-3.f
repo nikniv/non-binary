f9c7cff6a8840dc9bbbb6863e6d4ebc5235c7b53
niveditn <11422270+niveditn@users.noreply.github.com>
2019-04-09 12:56:12 +0530

    Update stripe-3.f

--- stripe-3.f
+++ stripe-3.f
@@ -1,3 +1,9 @@
+integer i, j
+
 program hello
-  print *, "Hello Purple!"
+  do i = 1, 100, 7
+     j = i*i
+     print *, "Hello Purple!"
+     print *, "i = ", i, ", i^2 = ", j             
+  end do
 end program hello
